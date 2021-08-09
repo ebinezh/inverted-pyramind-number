bd3e0ad7d0ca96465494111d82f3d5b41c24a4f1
ebinezh <88050562+ebinezh@users.noreply.github.com>
2021-08-09 21:54:46 +0530

    Add files via upload

---
+++ inverted pyramid of num.py
@@ -0,0 +1,11 @@
+# Inverted Pyramid of Numbers
+row = int(input("Enter value: "))
+b = 0
+for i in range(row, 0, -1):
+    b += 1
+    for j in range(1, i+1):
+        print(b, end=" ")
+    print("\r")
+
+
+#Dream2code
